a17cae884ea4aa0bc6b702fb39b1463fba20e252
Ayrat Kalimov <ayrat.klim1@yandex.ru>
2022-04-29 17:22:35 +0300

    added a script of switching tabs on the main page of the site

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
-// Модальное окно
+document.addEventListener('DOMContentLoaded', () => {
 
-const btnShowModal = document.querySelector('.btn_white'),
-      btnCloseModal = document.querySelector('.modal__close'),
-      modal = document.querySelector('.modal');
+    // Модальное окно
+
+    const btnShowModal = document.querySelector('.btn_white'),
+        btnCloseModal = document.querySelector('.modal__close'),
+        modal = document.querySelector('.modal');
 
 
-btnShowModal.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
+    btnShowModal.addEventListener('click', () => {
+        modal.style.display = 'block';
+    });
 
-btnCloseModal.addEventListener('click', () => {
-    modal.style.display = 'none';
+    btnCloseModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    // Табы
+    
+    const tabs = document.querySelectorAll('.tabheader__item'), // Пункты табов
+          tabContent = document.querySelectorAll('.tabcontent'), // Блоки табов
+          tabContainer = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+
+        tabContent.forEach((element) => {
+            element.style.display = 'none';
+        });
+
+        tabs.forEach((element) => {
+            element.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabContainer.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((element, i) => {
+                if (element == target) {           
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
 });
